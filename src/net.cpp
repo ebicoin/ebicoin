8420722a0c737a15f9bcba4cca50e92b914b055b
ebicoin-dev <dev@ebicoin.org>
2018-04-05 03:50:37 +0900

    Update seed

--- src/net.cpp
+++ src/net.cpp
@@ -1206,13 +1206,10 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"seedebicoin", "seed.ebicoin.org"},
-    {"seed", "seed.ebicoin.org"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"tseedebicoin", "tseed.ebicoin.org"},
     {NULL, NULL}
 };
 
@@ -1262,9 +1259,7 @@ void ThreadDNSAddressSeed()
 // Physical IP seeds: 32-bit IPv4 addresses: e.g. 178.33.22.32 = 0x201621b2
 unsigned int pnSeed[] =
 {
-    0x36a3b545, 0x3c1c26d8, 0x4031eb6d, 0x4d3463d1, 0x586a6854, 0x5da9ae65,
-    0x6deb7318, 0x9083fb63, 0x961bf618, 0xcabd2e4e, 0xcb766dd5, 0xdd514518,
-    0xdff010b8, 0xe9bb6044, 0xedb24a4c,
+    0x216a18d4,
 };
 
 void DumpAddresses()
